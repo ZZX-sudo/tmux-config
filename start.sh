14ea18553de386804ec1eeaddcf134371cb0050f
ZZX-sudo <771879894@qq.com>
2020-11-24 12:25:41 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 release=`lsb_release -i`
-path="~/tmux-vim-conf"
+path="${HOME}/tmux-vim-conf"
 
 case "$release" in
     "Ubuntu")
@@ -14,11 +14,11 @@ esac
 mkdir "$path" 
 git clone https://github.com/ZZX-sudo/tmux-vim-config.git "$path" 
 
-[[ -e "~/.vim.bk" ]] && echo "backup failure, please backup manually";exit
-[[ -e "~/.vimrc" ]] && cp ~/.vim ~/.vim.bk
-[[ -e "~/.tmux.conf.bk" ]] && echo "backup failure, please backup manually";exit
-[[ -e "~/.tmux.conf" ]] && cp ~/.tmux.conf ~/.tmux.conf.bk
-[[ -e "~/.vim.bk" ]] && echo "backup failure, please backup manually";exit
+[[ -e "${HOME}/.vim.bk" ]] && echo "backup failure, please backup manually";exit
+[[ -e "${HOME}/.vimrc" ]] && cp ~/.vim ~/.vim.bk
+[[ -e "${HOME}/.tmux.conf.bk" ]] && echo "backup failure, please backup manually";exit
+[[ -e "${HOME}/.tmux.conf" ]] && cp ~/.tmux.conf ~/.tmux.conf.bk
+[[ -e "${HOME}/.vim.bk" ]] && echo "backup failure, please backup manually";exit
 
 ln -s "$path/.vimrc" ~/
 ln -s "$path/.tmux.conf" ~/
