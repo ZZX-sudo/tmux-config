827b2765a44b8ed1404ce4f8d8fbf8f52ea2e9b2
ZZX-sudo <771879894@qq.com>
2023-02-21 23:53:38 +0800

    delete vim config

--- start.sh
+++ start.sh
@@ -1,30 +1,16 @@
-#!/usr/bin/bash
-release=`lsb_release -i | awk '{print $3}'`
+#!/usr/bin/env bash
+
 path="${HOME}/tmux-vim-conf"
-
-case "$release" in
-    "Ubuntu")
-        sudo apt update && sudo apt install python3-autopep8 flake8 python3-ipdb 
-        ;;
-    "Arch")
-        sudo pacman -Syy flake8 autopep8 python-ipdb 
-        ;;
-esac
-
+if [[ -d "$path" ]];then
+    mv "$path" "$path"_bak
+fi
 mkdir "$path" 
 git clone https://github.com/ZZX-sudo/tmux-vim-config.git "$path" 
 
-[[ -e "${HOME}/.vim.bk" ]] && {echo "backup failure, please backup manually";exit}
-[[ -e "${HOME}/.vimrc" ]] && cp ~/.vim ~/.vim.bk
-[[ -e "${HOME}/.tmux.conf.bk" ]] && {echo "backup failure, please backup manually";exit}
-[[ -e "${HOME}/.tmux.conf" ]] && cp ~/.tmux.conf ~/.tmux.conf.bk
-[[ -e "${HOME}/.vim.bk" ]] && {echo "backup failure, please backup manually";exit}
 
-ln -s "$path/.vimrc" ~/
+[[ -e "${HOME}/.tmux.conf" ]] && {
+   cp ~/.tmux.conf ~/.tmux.conf.bk
+}
+
 ln -s "$path/.tmux.conf" ~/
 ln -s "$path/.tmux" ~/
-ln -s "$path/.vim" ~/.vim
-
-
-git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
-vim +PluginInstall
